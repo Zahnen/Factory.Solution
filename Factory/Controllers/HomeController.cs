92f03eb7d75fe32ba62fe7feba9fc2b2185f2b22
Zahnen Garner <zahnen@gmail.com>
2021-01-08 16:15:48 -0800

    add Directory route to HomeController

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+        _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
       return View();
+    }
+
+    [HttpGet("/directory/")]
+    public ActionResult Directory()
+    {
+      ViewBag.Engineers = _db.Engineers.ToList();
+      ViewBag.Machines = _db.Machines.ToList();
+      return View();    
     }
   }
 }
